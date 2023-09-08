53a88eacdbfdfd8fb8db52ff9c283861b1f57ce7
Matus Mastena <Shadiness9530@proton.me>
2023-09-08 18:40:09 +0200

    Going to implement the functionality of this bot.

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,33 @@ pub mod commands {
     pub const HELP_TIMETABLE: &'static str = "!rozvrh";
     pub const HELP_HWS: &'static str = "!domaceulohy";
 }
+pub mod functionality {
+    #[async_trait]
+    impl EventHandler for Handler {
+        async fn message(&self, ctx: Context, msg: Message) {
+            if msg.content == HELP_COMMAND {
+                if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+        }
+        /// Some OOP
+        async fn ready(&self, _: Context, ready: Ready) {
+            println!("{} is connected!", ready.user.name);
+        }
+    }
+}
+/// Initialize the bot
+async fn init_the_bot() {
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
