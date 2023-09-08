6885ca3437d1027ceb0f772061affbe610622817
Matus Mastena <Shadiness9530@proton.me>
2023-09-08 18:33:19 +0200

    Finished all the important commands.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-///# A Discord bot for the most **elistist** mid school in Bratislawa
-/// uwu
-///## What's here:
+///# A Discord bot for the most **elistist** mid school in Bratislawa (UwU)
+///# What's here:
 ///- Bryndzové halušky (just kidding, they aren't here)
-///- Rust code :tick:
+///- Rust code (obviously)
+///- Connections to the Discord and EduPage API through unofficial implementation made by
+/// [@loumadev](https://github.com/loumadev)
 pub mod commands {
     pub const HELP_MESSAGE: &'static str = "!pomoc";
     pub const VIEW_MARKS: &'static str = "!pozriznamky";
     pub const VIEW_MSGS: &'static str = "!spravy";
-    pub const HELP_MESSAGE: &'static str = "!pomoc";
-    pub const HELP_MESSAGE: &'static str = "!pomoc";
+    pub const HELP_TIMETABLE: &'static str = "!rozvrh";
+    pub const HELP_HWS: &'static str = "!domaceulohy";
 }
